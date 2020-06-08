65d883aa8a1aaa4feb5a7914fde92b6a3964253d
matthewfeatherston5 <matthewfeatherston5@gmail.com>
2020-06-08 06:52:39 -0700

    Add files via upload

---
+++ pointers1.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+static int var1 = 5;
+
+class Person {
+public:
+
+	char Name,  Sex;
+
+	Person(char aName, char aSex)
+	{
+		Name = aName;
+		Sex = aSex;
+	}
+};
+
